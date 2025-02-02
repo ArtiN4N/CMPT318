ec059b97dad168d837f2273eb4fe1ae438765001
Katya Kubyshkin <klk6@sfu.ca>
2025-02-02 15:55:49 -0800

    Most recent version
    Forgot to push the most recent version of the file on my last commit lol sorry

--- a2/a2.R
+++ a2/a2.R
@@ -6,8 +6,47 @@ library(dplyr)
 library(lubridate)
 library(zoo)
 
-data <- df_zscores  # Store preprocessed data 
-data$weekStart <- floor_date(data$DateTime, unit = "week", week_start = 1)
-window_size = 10
+#---------------------------- Moving Average ------------------------------
 
-# Moving average 
+df_smoothened <- df_zscores
+
+# Identify week start date for each row 
+df_smoothened$weekStart <- floor_date(
+  df_smoothened$DateTime,
+  unit = "week",
+  week_start = 1)
+
+# Slice dataset into complete weeks 
+df_smoothened <- df_smoothened %>%
+  group_by(weekStart) %>%
+  filter(n_distinct(Date) == 7) %>%
+  ungroup()
+
+# Initialize smoothened_Global_intensity column
+df_smoothened$smoothened_Global_intensity <- NA
+
+window_size <- 7
+
+# Compute moving average 
+for (week in unique(df_smoothened$weekStart)) {
+  # Rows for the current week
+  rows <- df_smoothened$weekStart == week
+  smooth_values <- rollmean(df_smoothened$Global_intensity[rows],
+                          k = window_size,
+                          fill = NA,
+                          align = "center")
+  
+  df_smoothened$smoothened_Global_intensity[rows] <- smooth_values
+}
+
+# Each smoothened week is a dataframe 
+smoothened_weeks <- split(df_smoothened, data$weekStart)
+
+# Rename the weeks from: smoothened_week_1 to smoothened_week_n
+names(smoothened_weeks) <- paste0(
+  "smoothened_week_",
+  seq_along(smoothened_weeks))
+
+#------------------------- Average Smoothened Week ------------------------
+
+#----------------------- Most & Least Anomalous Weeks ---------------------
